e584bffb8f1e32da244eff89023553cfd9e7e19f
Rafal Mi <rafal.m4@gmail.com>
2019-08-17 22:33:45 +0200

    Defining the Router Animation

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <transition name="router-anim" enter-active-class="animated fadeInDown" leave-active-class="animated fadeOutDown">
+    <transition name="router-anim">
     <router-view/>
     </transition>
   </div>
@@ -34,5 +34,34 @@
     position: fixed;
     width: inherit;
   }
+
+  .router-anim-enter-active {
+  animation: coming 1s;
+  animation-delay: .5s;
+  opacity: 0;
+}
+.router-anim-leave-active {
+  animation: going 1s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+}
+@keyframes coming {
+  from {
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
 
 </style>
